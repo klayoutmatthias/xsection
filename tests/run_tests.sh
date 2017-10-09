0bd020de5eae6aa8efb5da5f1de280e068df2a86
Matthias Koefferlein <matthias@koefferlein.de>
2017-10-09 23:23:30 +0200

    Fixed tests, refactored so deployment won't contain doc and tests.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,7 +9,7 @@ mkdir -p run_dir
 
 failed=""
 
-bin=../src/xsection.lym
+bin=../src/macros/xsection.lym
 
 for tc_file in *.xs; do
 
@@ -18,7 +18,7 @@ for tc_file in *.xs; do
   echo "---------------------------------------------------"
   echo "Running testcase $tc .."
 
-  klayout -z -rd xs_run=$tc.xs -rd xs_cut="-1,0;1,0" -rd xs_out=run_dir/$tc.gds xs_test.gds -r $bin 
+  klayout -rx -z -rd xs_run=$tc.xs -rd xs_cut="-1,0;1,0" -rd xs_out=run_dir/$tc.gds xs_test.gds -r $bin 
 
   if klayout -b -rd a=au/$tc.gds -rd b=run_dir/$tc.gds -rd tol=10 -r run_xor.rb; then
     echo "No differences found."
